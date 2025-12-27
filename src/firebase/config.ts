d7a3d53b899c73c9cbc98c3c3d078f4a9321c415
naeel <vapefuel01@gmail.com>
2025-12-27 21:52:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4016393560-dc903",
+  "appId": "1:962833836994:web:2e27fc1b15174bdd9c445b",
+  "apiKey": "AIzaSyA_yPTg6Cmk0pP5eDm9k49TI4R_KvH-mAs",
+  "authDomain": "studio-4016393560-dc903.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "962833836994"
+};
