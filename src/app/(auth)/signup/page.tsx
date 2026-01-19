d896224212a079836e0a9cbe7f4eb412270f2573
naeel <vapefuel01@gmail.com>
2026-01-19 16:35:33 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -10,6 +10,7 @@ import { useRouter } from 'next/navigation';
 import { GlassForm } from "@/components/ui/glass-form";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
 import { useFirebase } from "@/firebase";
 import { createUserWithEmailAndPassword, User } from "firebase/auth";
